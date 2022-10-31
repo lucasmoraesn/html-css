7ea6adea8c665cb8abd3133c520c929ea6f69e0e
Lucas Nascimento <lucasmoraesn@hotmail.com>
2022-10-31 10:32:09 -0300

    Initial commit

---
+++ exercícios/ex002/index.h
@@ -0,0 +1,27 @@
+const Discord = require("discord.js")
+const fetch = require("node-fetch")
+const client = new Discord.Client()
+
+function getQuote() {
+  return fetch("https://zenquotes.io/api/random")
+    .then(res => {
+      return res.json()
+      })
+    .then(data => {
+      return data[0]["q"] + " -" + data[0]["a"]
+    })
+}
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`)
+})
+
+client.on("message", msg => {
+  if (msg.author.bot) return
+    
+  if (msg.content === "$inspire") {
+    getQuote().then(quote => msg.channel.send(quote))
+  }
+})
+
+client.login(process.env.TOKEN)
